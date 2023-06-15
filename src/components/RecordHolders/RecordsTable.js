fc2732055f67afbae56460260b1c7407df0ee901
alinakornieieva <alinakorneeva401@gmail.com>
2023-06-15 16:53:35 +0300

    add error and loading processing, add style to RecordsTable

--- src/components/RecordHolders/RecordsTable.js
+++ src/components/RecordHolders/RecordsTable.js
@@ -4,8 +4,10 @@ import './RecordTable.css'
 const RecordsTable = ({data: changeData}) => {
     const [data, setData] = useState([])
     const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         const fetchData = async () => {
+            setLoading(true)
             await fetch('http://localhost:5000/get-record-holders')
             .then((data) => data.json())
             .then((res) => setData(res))
@@ -13,19 +15,30 @@ const RecordsTable = ({data: changeData}) => {
                 setError(true)
                 console.error(e)
             })
+            setLoading(false)
         }
         fetchData()
     }, [changeData])
     const records = data.sort((a, b) => b.score - a.score).slice(0, 5)
+    if (error) {
+        return <div className="error">Something went wrong...</div>
+    }
+    if (loading) {
+        return <div className="loading">Loading...</div>
+    }
     return <table>
+        <thead>
             <tr>
                 <th>Name</th>
                 <th>Score</th>
             </tr>
-        {records?.map((record) => <tr key={record.id}>
-            <td>{record.name}</td>
-            <td>{record.score}</td>
-        </tr>)}
+        </thead>
+        <tbody>
+            {records?.map((record) => <tr key={record.id}>
+                <td>{record.name}</td>
+                <td>{record.score}</td>
+            </tr>)}
+        </tbody>
     </table>
 }
 
